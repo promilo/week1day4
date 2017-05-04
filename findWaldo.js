caa6b60448a996b838e83fdcf0313a2c531e74ce
promilo <milind.shah@mail.utoronto.ca>
2017-05-04 15:20:14 +0000

    replaced for loop with for each

--- findWaldo.js
+++ findWaldo.js
@@ -1,13 +1,16 @@
 function findWaldo(arr, found) {
-  for (var i = 0; i < arr.length; i++) {
-    if (arr[i] === "Waldo") {
-      found(i);   // execute callback
+  arr.forEach(found);
+  // for (var i = 0; i < arr.length; i++) {
+  //   if (arr[i] === "Waldo") {
+  //     found(i);   // execute callback
     }
-  }
+
+function actionWhenFound(currentValue, i) {
+  if (currentValue === "Waldo") {
+       // execute callback
+       console.log("Found him at index " + i);
+}
 }
 
-function actionWhenFound(i) {
-  console.log("Found him at index " + i);
-}
 
 findWaldo(["Alice", "Bob", "Waldo", "Winston"], actionWhenFound);
